be57706575faf357d898ff5d2b00ee28e59fdbed
Psylo2 <psylo20@hotmail.com>
2022-02-13 02:21:00 +0200

    update factory

--- factory.py
+++ factory.py
@@ -1,23 +1,29 @@
-from repository import UserRepository, MessageRepository, LogRepository
-
+from repository.controllers import UserRepositoryController, MessageRepositoryController, LogRepositoryController
+from manager import FieldsValidation
 
 class Factory:
     def __init__(self):
         self._user_repository = None
         self._message_repository = None
         self._logs_repository = None
+        self._field_validation = None
 
-    def get_user_repository(self) -> UserRepository:
+    def get_user_repository(self) -> UserRepositoryController:
         if not self._user_repository:
-            self._user_repository = UserRepository()
+            self._user_repository = UserRepositoryController()
         return self._user_repository
 
-    def get_log_repository(self) -> LogRepository:
+    def get_log_repository(self) -> LogRepositoryController:
         if not self._logs_repository:
-            self._logs_repository = LogRepository()
+            self._logs_repository = LogRepositoryController()
         return self._logs_repository
 
-    def get_message_repository(self) -> MessageRepository:
+    def get_message_repository(self) -> MessageRepositoryController:
         if not self._message_repository:
-            self._message_repository = MessageRepository()
+            self._message_repository = MessageRepositoryController()
         return self._message_repository
+
+    def get_field_validation(self) -> FieldsValidation:
+        if not self._field_validation:
+            self._field_validation = FieldsValidation()
+        return self._field_validation

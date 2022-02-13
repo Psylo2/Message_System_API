1c7c63133e9ce014a8843b72071c01b621758622
Psylo2 <psylo20@hotmail.com>
2022-02-13 02:22:00 +0200

    update user resource

--- resources/user_resource.py
+++ resources/user_resource.py
@@ -1,14 +1,7 @@
-from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import (jwt_required, get_raw_jwt,
-                                get_jwt_identity, create_access_token,
-                                jwt_refresh_token_required,
-                                create_refresh_token)
+from flask_jwt_extended import jwt_required, jwt_refresh_token_required
 from flask_restful import Resource, reqparse
-from libs.blacklist import BLACKLIST
 from libs.strings import gettext
-from libs.inputs_validation import valid_email, valid_username,  valid_password
-from models.user_model import UserModel
-from models.log import LogModel
+
 
 """User:      [*] Register
               [*] Login

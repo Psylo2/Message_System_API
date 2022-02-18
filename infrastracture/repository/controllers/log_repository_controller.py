f7e46d94e682954d3d14aabb81bf3fa024adbb8d
Psylo2 <psylo20@hotmail.com>
2022-02-19 00:36:20 +0200

    move log repository and services to infrastructure

--- infrastracture/repository/controllers/log_repository_controller.py
+++ infrastracture/repository/controllers/log_repository_controller.py
@@ -1,9 +1,9 @@
 from typing import Dict, List
 
 from manager import RepositoryManager
-from repository.repository import repository
-from repository.log_repository import LogRepository
-from repository.services import LogRepositoryService
+from infrastracture.repository.repository import repository
+from infrastracture.repository.log_repository import LogRepository
+from infrastracture.repository.services import LogRepositoryService
 
 
 class LogRepositoryController(RepositoryManager, LogRepositoryService):
@@ -11,29 +11,24 @@ class LogRepositoryController(RepositoryManager, LogRepositoryService):
         super().__init__()
 
     def save_log(self, log_data: Dict) -> LogRepository:
-        """Save Log to DB"""
         log = LogRepository(**log_data)
         log.add_to_repository()
         return log
 
     def find_by_log_id(self, id: int) -> LogRepository:
-        """Find All Logs of a User, order by newest"""
         return LogRepository.query.filter_by(id=id).first()
 
     def find_all_logs(self) -> List[LogRepository]:
-        """Find all Log by id, order by newest"""
         return LogRepository.query.all()
 
     def find_by_level(self, threat_lvl: str) -> LogRepository:
-        """Find all Log by Threat Level, order by newest"""
         return LogRepository.query.filter_by(threat_lvl=threat_lvl).order_by(repository.desc(LogRepository.id)).all()
 
     def find_user_logs(self, id: int) -> LogRepository:
-        """return all user's logs order by newest message"""
         return LogRepository.query.filter_by(user_id=id).order_by(repository.desc(LogRepository.id)).all()
 
     def insert_timestamp(self) -> float:
-        return self.insert_timestamp()
+        return super().insert_timestamp()
 
     def convert_timestamp(self, timestamp: float) -> str:
-        return self.convert_timestamp(timestamp=timestamp)
+        return super().convert_timestamp(timestamp=timestamp)

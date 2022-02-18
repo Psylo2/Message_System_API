228ff9af9424d3dc807b8b2009ca4eb99424f242
Psylo2 <psylo20@hotmail.com>
2022-02-19 00:38:24 +0200

    move repository and service to infrastructure

--- infrastracture/repository/repository.py
+++ infrastracture/repository/repository.py
@@ -2,11 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 class Repository:
-    def __init__(self):
-        pass
-
     def __new__(cls, *args, **kwargs):
         return SQLAlchemy()
 
 
-repository = Repository()
+repository: SQLAlchemy = Repository()

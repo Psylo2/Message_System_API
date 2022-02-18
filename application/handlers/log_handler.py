aacef84b2bdf8ea54459877beab731188ed1e22b
Psylo2 <psylo20@hotmail.com>
2022-02-19 00:34:34 +0200

    move log logic, use-case, interfaces to application

--- application/handlers/log_handler.py
+++ application/handlers/log_handler.py
@@ -1,5 +1,5 @@
-from usecases import LogUseCase
-from models import LogModel
+from application.usecases import LogUseCase
+from domain.models import LogModel
 
 class LogHandler(LogUseCase):
     def __init__(self, factory):
@@ -7,17 +7,17 @@ class LogHandler(LogUseCase):
 
     def low_priority_log(self, id: int, msg: str) -> None:
         create_at = self._logs_repository.insert_timestamp()
-        log = LogModel(user=id, action=msg, threat_lvl=LogModel.ThreatLevel.LOW, create_at=create_at)
-        self._logs_repository.save_log(log_data=log)
+        log = LogModel(user_id=id, action=msg, threat_lvl=LogModel.ThreatLevel.LOW.value, create_at=create_at)
+        self._logs_repository.save_log(log_data=log.dict())
 
     def medium_priority_log(self, id: int, msg: str) -> None:
         create_at = self._logs_repository.insert_timestamp()
-        log = LogModel(user=id, action=msg, threat_lvl=LogModel.ThreatLevel.MEDIUM, create_at=create_at)
-        self._logs_repository.save_log(log_data=log)
+        log = LogModel(user_id=id, action=msg, threat_lvl=LogModel.ThreatLevel.MEDIUM.value, create_at=create_at)
+        self._logs_repository.save_log(log_data=log.dict())
 
     def high_priority_log(self, id: int, msg: str) -> None:
         create_at = self._logs_repository.insert_timestamp()
-        log = LogModel(user=id, action=msg, threat_lvl=LogModel.ThreatLevel.HIGH, create_at=create_at)
-        self._logs_repository.save_log(log_data=log)
+        log = LogModel(user_id=id, action=msg, threat_lvl=LogModel.ThreatLevel.HIGH.value, create_at=create_at)
+        self._logs_repository.save_log(log_data=log.dict())
 
 
